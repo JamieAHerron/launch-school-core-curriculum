a05ae8f813ca42108855f21ecafe30ab0aed5dd9
Jamie Herron <jamieah.jh@gmail.com>
2025-05-04 19:05:55 +0800

    PY101 lesson 3 easy_1 questions

---
+++ lesson_3/easy_1/question_3.py
@@ -0,0 +1,8 @@
+#Show two different ways to create a new string with "Four score and
+# " prepended to the front of the string referenced by famous_words.
+
+famous_words = "seven years ago..."
+
+#method 1
+print(f'Four score and {famous_words}')
+print('Four score and ' + famous_words)
---
+++ lesson_3/easy_1/question_8.py
@@ -0,0 +1,8 @@
+#How can we add multiple items to our list (e.g., 'Dino' and 'Hoppy')? 
+#Replace the call to append with another method invocation.
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"]
+flintstones.extend(["Dino", "Hoppy"])
+
+print(flintstones)
+
