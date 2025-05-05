8a7d2b89933b85fc68293e1fb1336554d7133edb
Jamie Herron <jamieah.jh@gmail.com>
2025-05-05 19:17:11 +0800

    PY101 lesson 3 easy_2 first pass

---
+++ lesson_3/easy_2/question_8.py
@@ -0,0 +1,7 @@
+#Determine whether the following dictionary of people
+#and their age contains an entry for 'Spot':
+
+ages = {'Herman': 32, 'Lily': 30, 'Grandpa': 5843, 'Eddie': 10}
+
+print('Spot' in ages)
+
