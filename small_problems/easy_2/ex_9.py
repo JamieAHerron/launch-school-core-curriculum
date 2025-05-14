d1f501ebb931d0179cfc611aa566441e22c2b3e9
Jamie Herron <jamieah.jh@gmail.com>
2025-05-14 18:15:23 +0800

    Updated solutions small problem set PY101

---
+++ small_problems/easy_2/ex_9.py
@@ -0,0 +1,12 @@
+# Build a program that randomly generates and prints Teddy's age. To get the age, you should generate a random number between 20 and 100, inclusive.
+
+# Teddy is 69 years old!
+
+import random
+
+print(f'Teddy is {random.randint(20, 100)} years old!')
+
+#What is the difference between randrange and randint? 
+
+#Further Exploration
+# Modify this program to ask for a name, then print the age for that person. For an extra challenge, use "Teddy" as the name if no name is entered.
