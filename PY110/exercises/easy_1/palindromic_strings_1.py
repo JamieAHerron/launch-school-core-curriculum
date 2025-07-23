29ce9108e7e864752d4da1e7978e01c583b4aa5f
Jamie Herron <jamieah.jh@gmail.com>
2025-07-23 17:39:22 +0800

    Update LS exercises

---
+++ PY110/exercises/easy_1/palindromic_strings_1.py
@@ -0,0 +1,52 @@
+'''
+Write a function that returns True if the string passed as an argument is a palindrome, False otherwise. A palindrome reads the same forwards and backwards. For this problem, the case matters and all characters matter.
+
+Input: string
+Output: Boolean (True or False)
+
+Explicit requirements:
+    - Take a string as input
+    - Evaluate if the string is a palindrome 
+    - All characters in the string must be evaluated
+    - Out True or False as a boolean (is/is not a palindrome)
+
+Implicit requirements:
+    - Do I assume the input will always be a string?
+    - How to deal with empty strings/ non-string types?
+
+Data structure:
+    - string object (strings are iterable can use slicing)
+
+Algorithm:
+    - Compare input string with reversed version
+    - Use slicing to create reversed version of input string 
+    - if they match return True
+    - if not return False
+    - Use if statement to evaluate True or False
+'''
+
+def is_palindrome(string):
+    if isinstance(string, str) and string != '':
+        reverse_string = string[::-1]
+        if string == reverse_string:
+            return True
+        else:
+            return False
+    else:
+        return 'You entered an empty string or non-string type, try again.'
+
+# All of these examples should print True
+
+print(is_palindrome('madam') == True)
+print(is_palindrome('356653') == True)
+print(is_palindrome('356635') == False)
+
+# case matters
+print(is_palindrome('Madam') == False)
+
+# all characters matter
+print(is_palindrome("madam i'm adam") == False)
+
+#outliers
+print(is_palindrome(5))
+print(is_palindrome(''))
