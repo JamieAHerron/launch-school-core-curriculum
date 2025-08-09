f4e0d46e12a7a1eea181b10b0fc10817b35f6a70
Jamie Herron <jamieah.jh@gmail.com>
2025-08-09 18:34:15 +0800

    Updated LS exercises

---
+++ PY110/exercises/easy_2/combine_two_lists.py
@@ -0,0 +1,14 @@
+'''
+Write a function that combines two lists passed as arguments and returns a new list that contains all elements from both list arguments, with each element taken in alternation.
+
+You may assume that both input lists are non-empty, and that they have the same number of elements.
+'''
+
+def interleave(lst1, lst2):
+    return [item for pair in zip(lst1, lst2)
+                 for item in pair]
+
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+expected = [1, "a", 2, "b", 3, "c"]
+print(interleave(list1, list2))      # True
